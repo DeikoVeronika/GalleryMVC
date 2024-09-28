0289f89f7ba2b587c6de680d12ad09a2aa44a083
Veronika Deiko <deykonika2005@gmail.com>
2024-09-28 12:05:13 +0300

    updated landing page for photos and added map

--- GalleryDomain/Model/Location.cs
+++ GalleryDomain/Model/Location.cs
@@ -10,9 +10,16 @@ public partial class Location : Entity
     [Display(Name = "Локація")]
     public string Name { get; set; } = null!;
 
-    [Display(Name = "Посилання на локацію")]
+    [Display(Name = "Посилання на інформацію (опціонально)")]
+    [Url(ErrorMessage = "Будь ласка, введіть дійсний URL.")]
+
     public string? Link { get; set; }
+    [Required(ErrorMessage = "Введіть широту")]
+
+    [Display(Name = "Широта")]
     public double Latitude { get; set; } // Широта
+    [Required(ErrorMessage = "Введіть довготу")]
+    [Display(Name = "Довгота")]
     public double Longitude { get; set; } // Довгота
 
     public virtual ICollection<Photo> Photos { get; set; } = new List<Photo>();
